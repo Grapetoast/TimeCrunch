84202a73424d28386d4f29818884e08c7b579c04
Grapetoast <bonham.goodman@gmail.com>
2018-02-05 13:54:38 -0700

    init

---
+++ templates/component.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="main"></div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped lang="less">
+.main {
+
+}
+</style>
