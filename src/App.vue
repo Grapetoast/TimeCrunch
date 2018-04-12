398de10bef46e074c7e53e9e2550745075eb9ac3
Spiralis-Solutions <krisstjohn96@gmail.com>
2018-04-12 14:47:11 -0700

    Revert "added js interface"
    This reverts commit 7dd233c71f0f03e186f123ecd0258d8bd34a6087.

--- src/App.vue
+++ src/App.vue
@@ -106,7 +106,6 @@ export default {
         vue.user.id = user.data.id
         vue.user.admin = user.data.admin
         vue.logged = true
-        window.subscribe(vue.user.token, vue.user.companyId)
         vue.$router.push('/')
       } else {
         vue.logged = false
